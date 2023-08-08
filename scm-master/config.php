19c05dca0748f4e365f8e71551af02b344d718ac
monalisapattanayak-ind <141707842+monalisapattanayak-ind@users.noreply.github.com>
2023-08-08 12:27:48 +0530

    Add files via upload

---
+++ scm-master/config.php
@@ -0,0 +1,15 @@
+<?php
+$conn_error='Could not Connect.';
+$mysql_host='localhost';
+$mysql_user='root';
+$mysql_pass='';
+$mysql_db='sourcecodester_ scm_new';
+
+$conn=mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
+
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+	
+?>
